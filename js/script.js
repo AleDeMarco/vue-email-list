687bd393af0b218c5f9a1b399d55d658770864e1
Alessandro De Marco <lookatde.lad@gmail.com>
2021-02-08 13:15:07 +0100

    Esercizio terminato

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    mail: []
+  },
+  mounted(){
+    for (let i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+          this.mail.push(result.data.response);
+          console.log(result.data.response);
+        })
+    }
+  }
+});
